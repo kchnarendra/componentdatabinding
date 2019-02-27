a4e93ab450d4cc1d8e116065d559ffc054ff7914
Narendra Chaudhary <>
2019-02-28 01:45:25 +0530

    Your Message

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'just a test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+  onBluePrintAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onChangeFirst(){
+    this.serverElements[0].name = 'changed!';
+  }
+
+  onDestroyfirst(){
+    this.serverElements.splice(0, 1);
+  }
+}
